447fe5d1d92c16735d89364c2146c50925b00ee3
Sylvia Stone <sylviaestone@gmail.com>
2025-08-06 18:55:05 -0500

    Attempting to convert to c#

---
+++ scenes/Player.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 100.0f;
+
+	public override void _PhysicsProcess(float delta)
+	{
+		PlayerMovement(delta);
+	}
+
+	public void PlayerMovement(float delta)
+	{
+		Vector2 velocity = Velocity;
+
+		if (Input.IsActionPressed("ui_right"))
+		{
+			velocity.X = Speed;
+			velocity.Y = 0;
+		}
+		else if (Input.IsActionPressed("ui_left"))
+		{
+			velocity.X = -Speed;
+			velocity.Y = 0;
+		}
+		else if (Input.IsActionPressed("ui_up"))
+		{
+			velocity.X = 0;
+			velocity.Y = Speed;
+		}
+		else if (Input.IsActionPressed("ui_down"))
+		{
+			velocity.X = 0;
+			velocity.Y = -Speed;
+		}
+
+		Velocity = velocity;
+
+		MoveAndSlide();
+	}
+}
