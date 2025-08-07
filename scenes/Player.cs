eb8e7e03a7e543e1111b1cff8b2686769eb55624
Sylvia-Stone <sylviaestone@gmail.com>
2025-08-06 21:57:36 -0500

    Fix namespace

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -1,42 +1,46 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody2D
+namespace FirstActionRPG.scenes
 {
-	public const float Speed = 100.0f;
-
-	public override void _PhysicsProcess(double delta)
+	public partial class Player : CharacterBody2D
 	{
-		PlayerMovement(delta);
-	}
+		public const float Speed = 100.0f;
 
-	public void PlayerMovement(double delta)
-	{
-		Vector2 velocity = Velocity;
-
-		if (Input.IsActionPressed("ui_right"))
+		public override void _PhysicsProcess(double delta)
 		{
-			velocity.X = Speed;
-			velocity.Y = 0;
-		}
-		else if (Input.IsActionPressed("ui_left"))
-		{
-			velocity.X = -Speed;
-			velocity.Y = 0;
-		}
-		else if (Input.IsActionPressed("ui_up"))
-		{
-			velocity.X = 0;
-			velocity.Y = -Speed;
-		}
-		else if (Input.IsActionPressed("ui_down"))
-		{
-			velocity.X = 0;
-			velocity.Y = Speed;
+			PlayerMovement(delta);
 		}
 
-		Velocity = velocity;
+		public void PlayerMovement(double delta)
+		{
+			Vector2 velocity = Velocity;
 
-		MoveAndSlide();
+			if (Input.IsActionPressed("ui_right"))
+			{
+				velocity.X = Speed;
+				velocity.Y = 0;
+			}
+			else if (Input.IsActionPressed("ui_left"))
+			{
+				velocity.X = -Speed;
+				velocity.Y = 0;
+			}
+			else if (Input.IsActionPressed("ui_up"))
+			{
+				velocity.X = 0;
+				velocity.Y = -Speed;
+			}
+			else if (Input.IsActionPressed("ui_down"))
+			{
+				velocity.X = 0;
+				velocity.Y = Speed;
+			}
+
+			Velocity = velocity;
+
+			MoveAndSlide();
+		}
 	}
 }
+
